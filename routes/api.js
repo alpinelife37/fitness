2a3bf7001af6d5f420d60c388060477ca538432e
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-02-19 13:35:29 -0800

    updated api routes

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
 
-router.get("/api/workouts", (req, res) => {
-  Workout.find({}).then(workoutResults => {
-    console.log(workoutResults);
-    res.json(workoutResults);
-  });
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(workoutResults => {
+      console.log(workoutResults);
+      res.json(workoutResults);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(workoutResults => {
+      res.json(workoutResults);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 module.exports = router;
