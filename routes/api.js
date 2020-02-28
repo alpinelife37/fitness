252a74b3f84f0a4197b60d7c1dca5a7964318517
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-02-28 11:03:50 -0800

    added put/get routes

--- routes/api.js
+++ routes/api.js
@@ -13,9 +13,35 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(workoutResults => {
+      res.json(workoutResults);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
   Workout.find()
     .then(workoutResults => {
+      res.json(workoutResults);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then(workoutResults => {
+      console.log(workoutResults);
       res.json(workoutResults);
     })
     .catch(err => {
